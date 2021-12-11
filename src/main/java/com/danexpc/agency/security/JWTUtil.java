780977c3a20b346aa7bd5d356bc8a43fab40d64d
Danylo Smahliuk <danylosmahliuk@gmail.com>
2021-12-11 19:00:12 +0200

    Add slf4j logger to project

--- src/main/java/com/danexpc/agency/security/JWTUtil.java
+++ src/main/java/com/danexpc/agency/security/JWTUtil.java
@@ -7,6 +7,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.UnsupportedJwtException;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
@@ -17,6 +18,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 public class JWTUtil {
 
     private final static String SECRET_KEY = "secret_key";
@@ -62,13 +64,13 @@ public class JWTUtil {
             decodeJWT(authToken);
             return true;
         } catch (MalformedJwtException e) {
-//            log.error("Invalid JWT token: {}", e.getMessage());
+            log.error("Invalid JWT token: {}", e.getMessage());
         } catch (ExpiredJwtException e) {
-//            log.error("JWT token is expired: {}", e.getMessage());
+            log.error("JWT token is expired: {}", e.getMessage());
         } catch (UnsupportedJwtException e) {
-//            log.error("JWT token is unsupported: {}", e.getMessage());
+            log.error("JWT token is unsupported: {}", e.getMessage());
         } catch (IllegalArgumentException e) {
-//            log.error("JWT claims string is empty: {}", e.getMessage());
+            log.error("JWT claims string is empty: {}", e.getMessage());
         }
 
         return false;
